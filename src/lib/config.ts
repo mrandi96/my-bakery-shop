25e1ac33f27edfa1c920da764aab9b337ca4d991
Muhammad Randi Pratama <mrandi96@gmail.com>
2026-04-29 04:24:10 +0700

    Update Instagram URL and handle

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
 	siteUrl: "https://kuekito.com",
 	whatsappNumber: "6282183904974",
 	whatsappMessageTemplate: "Halo Kue Kito, saya mau pesan [Nama Produk] silakan infokan harganya ya. Terima kasih",
-	instagramUrl: "https://instagram.com/kuekito",
+	instagramUrl: "https://www.instagram.com/kue.kito.bengkulu/",
 	facebookUrl: "https://facebook.com/kuekito",
 	googleMapsUrl: "https://maps.app.goo.gl/9PudFJXqCMpudH9X9",
 	location: "Jl. Cimanuk Komplek Cimanuk Indah No. 10 RT 5 RW 2 Kec. Gading Cempaka, Bengkulu",
